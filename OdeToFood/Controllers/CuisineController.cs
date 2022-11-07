97fb4e74be306aed8c851c9d250b38880b8b5206
Salty <vertixo.vider05@gmail.com>
2022-11-07 19:50:19 +0200

    Add custom LogAttribute to CuisineController

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.AspNetCore.Authorization;
+using OdeToFood.Filters;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
@@ -7,15 +9,27 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    [Log]
     public class CuisineController : Controller
     {
+        //[Authorize]
         public IActionResult Search(string name = "unknown")
         {
             var message = HtmlEncoder.Default.Encode(name);
 
             //return Content(message);
             //return new EmptyResult();
-            return File("/css/site.css", "text/css");
+            //return File("/css/site.css", "text/css");
+            //return Json(HtmlEncoder.Default);
+            //return Redirect("https://www.tthk.ee");
+            //return RedirectToRoute("default",new { controller = "cuisine", action = "Second", count = 5 });
+            //return RedirectToAction(nameof(Second), new { count = 5 });
+            return RedirectToRoute("default", new {controller = "Home", action = "About"});
+        }
+
+        public IActionResult Second(int count)
+        {
+            return Content($"Teine on siin! {count}");
         }
     }
 }
